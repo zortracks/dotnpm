3342d2000af8e4ee8077d26952931386144301f9
zortracks <zortracks@outlook.com>
2026-01-12 11:00:17 +0100

    Setting up base project

---
+++ tests/DotNpm.Tests/Program.cs
@@ -0,0 +1,35 @@
+using DotNpm.Tests.Shared.Components;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DotNpm.Tests {
+
+    public static class Program {
+
+        public static void Main(string[] args) {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment()) {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+            app.UseHttpsRedirection();
+
+            app.UseAntiforgery();
+
+            app.MapStaticAssets();
+            app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
